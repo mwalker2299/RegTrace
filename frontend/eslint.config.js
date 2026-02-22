36b84023bd77b0b259d0a8b6fc1c5a042e6e0515
Matthew Walker <mjwalker2299@gmail.com>
2026-02-22 13:46:35 +0000

    chore(frontend): Set up basic test for pipeline
    - Adds app directory (feature and shared to follow in first feature PR)
    - Adds vitest config and test setup file
    - Simplies App() component
    - Adds simple test of App component
    - Removes unneeded assets
    - Adds vitest eslint-plugin and updates config to make linter aware of vitest globals
    -  pnpm test now passes!

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -5,6 +5,7 @@ import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
 import prettier from "eslint-config-prettier";
+import vitest from "@vitest/eslint-plugin";
 import { defineConfig, globalIgnores } from "eslint/config";
 
 export default defineConfig([
@@ -37,6 +38,19 @@ export default defineConfig([
       // Prevent eslint from complaining about React not being in scope when using JSX
       "react/react-in-jsx-scope": "off",
       "react/jsx-uses-react": "off",
+    },
+  },
+  {
+    files: ["**/*.{test,spec}.{ts,tsx}"],
+    plugins: { vitest },
+    rules: {
+      ...vitest.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...vitest.environments.env.globals,
+      },
     },
   },
 ]);
