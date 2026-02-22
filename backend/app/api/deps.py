e9c8e3ca36aed03be0c9bdf3200876c519313ccf
Matthew Walker <mjwalker2299@gmail.com>
2026-02-22 20:02:29 +0000

    chore(backend): Use lru_cache getters instead of globals

--- backend/app/api/deps.py
+++ backend/app/api/deps.py
@@ -2,11 +2,11 @@ from collections.abc import Generator
 
 from sqlalchemy.orm import Session
 
-from app.db.session import SessionLocal
+from app.db.session import new_session
 
 
 def get_db() -> Generator[Session, None, None]:
-    db = SessionLocal()
+    db = new_session()
     try:
         yield db
     finally:
